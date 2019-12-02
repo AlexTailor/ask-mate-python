205dd9035d726e8e4bc3d1fdd64f80b79980ec52
lilkertesz <lil.kertesz@gmail.com>
2019-12-02 11:29:53 +0100

    corrected directory, file name

--- server.py
+++ server.py
@@ -1,3 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+
+@app.route('/list')
+def list_questions():
+    return render_template('list.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True,
+    )
