ce890b5fe3ceee726a0716ac5d411cb5bb397d4e
AlexTailor <szabbo87@gmail.com>
2019-12-03 11:22:23 +0100

    display a question half-done

--- connection.py
+++ connection.py
@@ -6,7 +6,7 @@ import csv
 import os
 
 DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/question.csv'
-
+DATA_FILE_PATH2 = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/answers.csv'
 
 def get_csv_data(question_id=None):
     questions = []
@@ -18,4 +18,15 @@ def get_csv_data(question_id=None):
             if question_id is not None and question_id == question['id']:
                 return question
     return questions
+
+def get_csv_data(question_id=None):
+    answers = []
+    with open(DATA_FILE_PATH2, encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            answer = dict(row)
+            answers.append(answer)
+            if question_id is not None and question_id == answer['id']:
+                return answer
+    return answers
 
